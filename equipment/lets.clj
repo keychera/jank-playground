1e20e6447a3acd162bde7ce77e67724a8346b345
keychera <kyogatama@gmail.com>
2026-03-13 23:32:06 +0900

    some bb prep

--- equipment/lets.clj
+++ equipment/lets.clj
@@ -8,34 +8,34 @@
 
 (defonce deps-basis (delay (b/create-basis {:project "deps.edn"})))
 
-;; adapted from https://blog.agical.se/en/posts/how-to-create-a-really-simple-clojureclr-dependency-tool/
-(defn unpack-jar [jar-path]
-  (println "unpacking" jar-path)
-  (let [deps-subdir (str/replace (fs/file-name jar-path) #".jar$" "")
-        deps-dir    (str "/app/dependencies/" deps-subdir)]
-    (fs/create-dirs deps-dir)
-    (fs/unzip jar-path deps-dir {:replace-existing true})
-    deps-dir))
+(defn build-module-path []
+  (str/join File/pathSeparator (keys (:classpath @deps-basis))))
+
+(defn prep-kondo [module-path]
+  (try
+    (b/process {:command-args ["clj-kondo" "--lint" module-path
+                               "--dependencies" "--copy-configs" "--skip-lint"]})
+    (catch Throwable err
+      (println "error when running clj-kondo! cause:" (:cause (Throwable->map err))))))
+
+(defn prep [{}]
+  (let [module-path (build-module-path)]
+    (prep-kondo module-path)))
 
 (defn jank
-  {:org.babashka/cli {:exec-args {:prep? false}
-                      :coerce {:prep? :bool}}}
-  [{:keys [prep?] :as opts}]
-  (println "setting up jank pseudo deps.edn project\n  with ops:" opts)
-  (let [classpath (into []
-                        (map (if prep?
-                               (fn [path]
-                                 (if (str/ends-with? path ".jar")
-                                   (unpack-jar path)
-                                   path))
-                               identity))
-                        (keys (:classpath @deps-basis)))
-        LOAD_PATH (str/join File/pathSeparator classpath)]
-    (when prep?
-      (try
-        (b/process {:command-args ["clj-kondo" "--lint" LOAD_PATH
-                                   "--dependencies" "--copy-configs" "--skip-lint"]})
-        (catch Throwable err
-          (println "error when running clj-kondo! cause:" (:cause (Throwable->map err))))))
-    (println "running jank!")
-    (b/process {:command-args ["jank" "--module-path" LOAD_PATH "run-main" "chera.hello"]})))
+  [{}]
+  (println "setting up jank pseudo deps.edn project")
+  (let [module-path (build-module-path)]
+    (b/process {:command-args ["jank" "--module-path" module-path "run-main" "chera.hello"]})))
+
+(defn jepl
+  [{}]
+  (let [module-path (build-module-path)]
+    (b/process {:command-args ["jank" "--module-path" module-path "repl"]})))
+
+(defn compile-jank
+  [{}]
+  (let [module-path (build-module-path)]
+    (b/process {:command-args ["jank" "--module-path" module-path
+                               "compile" "chera.hello"
+                               "-o" "hello.exe"]})))
